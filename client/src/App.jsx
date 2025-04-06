c11fce8e59cb894c4bd34f345830f7dfd1689136
asjadahamed77 <asjadahamed77@gmail.com>
2025-04-06 15:38:06 +0530

    created ui for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import RegisterUser from "./authentication/RegisterUser";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { Toaster } from "react-hot-toast";
+import Login from "./authentication/Login";
 
 function App() {
   const location = useLocation();
@@ -15,19 +16,24 @@ function App() {
   return (
     <>
       <Provider store={store}>
-        <Toaster position="top-center" reverseOrder={false} toastOptions={{
- 
-    className: '',
-    duration: 5000,
-    removeDelay: 1000,
-    style: {
-      background: '#010b16',
-      color: '#fff',}}
-    } />
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            className: "",
+            duration: 5000,
+            removeDelay: 1000,
+            style: {
+              background: "#010b16",
+              color: "#fff",
+            },
+          }}
+        />
         {!isAuthPage && <TopBar />}
         {!isAuthPage && <Navbar />}
         <Routes>
           <Route path="register-user" element={<RegisterUser />} />
+          <Route path="login" element={<Login />} />
         </Routes>
       </Provider>
     </>
