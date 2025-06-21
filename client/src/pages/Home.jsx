96d4597862ec9c578de8804b204a46fbe8287259
asjadahamed77 <ahamedasjad77@gmail.com>
2025-06-21 22:25:45 +0530

    Working on fix issue on the authentication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HomeCategories from '../components/HomeCategories'
+import { useDispatch } from 'react-redux'
+import { autheticated } from '../redux/slices/authSlice'
 
 
 const Home = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+   dispatch(autheticated())
+  },[dispatch])
   return (
     <div>
 
